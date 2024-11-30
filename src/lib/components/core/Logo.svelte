f736225bf31f79eae88a229346d5e3fdc79ee8fd
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-11-30 14:38:35 +0700

    fix: missing base path

--- src/lib/components/core/Logo.svelte
+++ src/lib/components/core/Logo.svelte
@@ -2,7 +2,7 @@
   import { base } from "$app/paths";
 </script>
 
-<a href="/">
+<a href="{base}/">
   <div class="flex items-center gap-4">
     <img src="{base}/logo.svg" alt="Logo" class="size-10" />
     <span class="font-semibold uppercase text-2xl tracking-[2.72px] select-none">ANKI</span>
